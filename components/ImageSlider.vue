24ed30deb231a6700c700db8915bad988bdbedb2
JGastager <jgastager@gmx.at>
2025-01-23 20:16:58 +0100

    dev

--- components/ImageSlider.vue
+++ components/ImageSlider.vue
@@ -1,11 +1,13 @@
 <template>
     <h2 class="mb-6">Images</h2>
     <div class="relative w-full flex gap-7 overflow-x-auto rounded">
-        <div v-for="(image, index) in images?.backdrops" :key="index">
-            <div class="h-120 w-fit overflow-hidden card">
-                <img v-if="image.file_path" :src="$getImageUrl(image.file_path, 'backdrop', 'w1280')" loading="lazy" alt="image" :height="image.height" :width="image.width" class="h-full w-auto object-cover">
+        <template v-for="(image, index) in images?.backdrops" :key="index">
+            <div v-if="image.iso_639_1 == 'en' || image.iso_639_1 == null">
+                <div class="h-120 w-fit overflow-hidden card">
+                    <img v-if="image.file_path" :src="$getImageUrl(image.file_path, 'backdrop', 'w1280')" loading="lazy" alt="image" :height="image.height" :width="image.width" class="h-full w-auto object-cover">
+                </div>
             </div>
-        </div>
+        </template>
         <div class="absolute right-0 top-0 h-full w-19 flex cursor-pointer items-center justify-center card opacity-0 transition-opacity duration-300 hover:opacity-100">
             <span class="i-ph-arrow-right-bold size-6"/>
         </div>
--- components/Player.vue
+++ components/Player.vue
@@ -1,22 +1,33 @@
 <template>
     <div id="player">
-        <iframe :src="`https://${config.public.streamProviderDomain}/embed/${props.type}?tmdb=${props.tmdbId}`" style="width: 100%; height: 100%;" frameborder="0" referrerpolicy="origin" allowfullscreen class="fixed left-0 top-0 h-full w-full" title="player"/>
+        <iframe :src="playerUrl" style="width: 100%; height: 100%;" frameborder="0" referrerpolicy="origin" allowfullscreen class="fixed left-0 top-0 h-full w-full" title="player"/>
     </div>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps({
-    type: {
-        type: String, // movie or series
-        required: true,
-    },
-    tmdbId: {
-        type: Number,
-        required: true,
-    }
-});
+const props = defineProps<{
+    type: 'movie' | 'tv';
+    tmdbId: number;
+    season?: number;
+    episode?: number;
+}>();
 
 const config = useRuntimeConfig();  // Access server-side config
+
+const playerUrl = computed(() => {
+    if (props.type === 'tv') {
+    let url = `https://${config.public.streamProviderDomain}/embed/${props.type}?tmdb=${props.tmdbId}`;
+        if (props.season) {
+            url += `&season=${props.season}`;
+        }
+        if (props.episode) {
+            url += `&episode=${props.episode}`;
+        }
+    return url;
+    } else {
+        return `https://${config.public.streamProviderDomain}/embed/${props.type}?tmdb=${props.tmdbId}`;
+    }
+});
 </script>
 
 <style>
