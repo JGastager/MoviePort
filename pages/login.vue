94be1b6b1f47803bf990ed559e1d950727eb1add
Michael Eder <eder.michi98@gmx.at>
2025-02-03 20:40:21 +0100

    add login page

---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Login</h1>
+  <button @click="createToken" class="button">Create Token</button>
+</template>
+<script lang="ts" setup>
+import {useTMDB} from "#imports";
+
+const {fetchTMDB} = useTMDB();
+async function createToken() {
+  console.log('create token')
+  await fetchTMDB('https://api.themoviedb.org/3/authentication/token/new', options)
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+}
+
+</script>
+<style lang="scss">
+
+</style>
