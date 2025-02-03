d277dec360c3fb628c8c13adc5d08bc772385e2b
Michael Eder <eder.michi98@gmx.at>
2025-02-03 20:55:29 +0100

    add login page

--- pages/login.vue
+++ pages/login.vue
@@ -9,11 +9,13 @@ const {fetchTMDB} = useTMDB();
 async function createToken() {
   console.log('create token')
   await fetchTMDB('/authentication/token/new')
-      .then(res => res.json())
-      .then(res => console.log(res))
+      .then(res => console.log(res.request_token))
+      .then(res => useFetch(`https://www.themoviedb.org/authenticate/${res?.request_token}?redirect_to=http://localhost:3000/profile`)
+          .then(res => console.log(res))
+          .catch(err => console.error(err))
+      )
       .catch(err => console.error(err));
 }
-
 </script>
 <style lang="scss">
 
