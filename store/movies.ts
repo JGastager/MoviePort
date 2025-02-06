7d453d27d886015a76939b32434bef19c6f7d454
Michael Eder <eder.michi98@gmx.at>
2025-02-06 21:28:27 +0100

    store setup

--- store/movies.ts
+++ store/movies.ts
@@ -1,8 +1,24 @@
 import { defineStore } from 'pinia';
+import {useRuntimeConfig} from "#app";
+
+const runtimeConfig = useRuntimeConfig();
+const BASEURL = runtimeConfig.public.BASE_URL as string;
 
 export const useMoviesStore = defineStore('moviesStore', {
     state: () => ({
+        popularMovies: [],
+        trendingMovies: [],
     }),
     actions: {
+        async fetchPopularMovies() {
+            const response = await fetch(BASEURL + '/movie/popular');
+            const data = await response.json();
+            this.popularMovies = data.results;
+        },
+        async fetchTrendingMovies(timeWindow: string = 'day') {
+            const response = await fetch(BASEURL + '/movie/trending' + timeWindow);
+            const data = await response.json();
+            this.trendingMovies = data.results
+        }
     }
 });
