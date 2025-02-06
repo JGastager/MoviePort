4adf44e7dfe9138ce73117e0040a52ac53b30ea7
Michael Eder <eder.michi98@gmx.at>
2025-02-06 21:00:05 +0100

    add store

---
+++ store/account.ts
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia';
+
+export const useAccountStore = defineStore('accountStore', {
+    state: () => ({
+    }),
+    actions: {
+    }
+});
---
+++ store/movies.ts
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia';
+
+export const useMoviesStore = defineStore('moviesStore', {
+    state: () => ({
+    }),
+    actions: {
+    }
+});
