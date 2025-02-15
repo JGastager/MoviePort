59a930b726f2b63bc89affe22c8fbe81d21ffa4d
Michael Eder <eder.michi98@gmx.at>
2025-02-15 23:22:18 +0100

    feat: add types for tv show, made store, remove fetchTMDB composable

--- store/shows.ts
+++ store/shows.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import type { TMDBImagesResponse, TMDBVideosResponse } from "~/types/general";
-import type { TMDBPopularTvShowsResponse } from "~/types/tvshowDetails";
+import type { TVSeasonDetailsResponse } from "~/types/season";
+import type { TMDBPopularTvShowsResponse, TMDBSimilarTvShowsResponse, TMDBTVCreditsResponse, TMDBTVShowDetails } from "~/types/tvshowDetails";
 
 export const useShowsStore = defineStore("showsStore", {
     state: () => ({
@@ -8,21 +9,35 @@ export const useShowsStore = defineStore("showsStore", {
         trendingShows: {} as TMDBPopularTvShowsResponse,
         tvShowImages: {} as TMDBImagesResponse,
         tvShowVideos: {} as TMDBVideosResponse,
+        tvShowDetails: {} as TMDBTVShowDetails,
+        tvShowCredits: {} as TMDBTVCreditsResponse,
+        tvShowSeasonDetails: {} as TVSeasonDetailsResponse,
+        similarTvShows: {} as TMDBSimilarTvShowsResponse,
     }),
     actions: {
         async fetchPopularShows() {
-            const response = await $fetch<TMDBPopularTvShowsResponse>("/api/tv/popular");
-            this.popularShows = response;
+            this.popularShows = await $fetch<TMDBPopularTvShowsResponse>("/api/tv/popular");
         },
         async fetchTrendingShows(timeWindow: string = "day") {
-            const response = await $fetch<TMDBPopularTvShowsResponse>(`/api/trending/tv/${timeWindow}`);
-            this.trendingShows = response;
+            this.trendingShows = await $fetch<TMDBPopularTvShowsResponse>(`/api/trending/tv/${timeWindow}`);
         },
-        async fetchTvShowImages(movieId: number) {
-            this.tvShowImages = await $fetch<TMDBImagesResponse>(`/api/tv/${movieId}/images`);
+        async fetchTvShowImages(showId: number) {
+            this.tvShowImages = await $fetch<TMDBImagesResponse>(`/api/tv/${showId}/images`);
         },
-        async fetchTvShowVideos(movieId: number) {
-            this.tvShowVideos = await $fetch<TMDBVideosResponse>(`/api/tv/${movieId}/videos`);
+        async fetchTvShowVideos(showId: number) {
+            this.tvShowVideos = await $fetch<TMDBVideosResponse>(`/api/tv/${showId}/videos`);
+        },
+        async fetchTvShowDetails(showId: number) {
+            this.tvShowDetails = await $fetch<TMDBTVShowDetails>(`/api/tv/${showId}`);
+        },
+        async fetchTvShowCredits(showId: number) {
+            this.tvShowCredits = await $fetch<TMDBTVCreditsResponse>(`/api/tv/${showId}/credits`);
+        },
+        async fetchTvShowSeasonDetails(showId: number, seasonNumber: number) {
+            this.tvShowSeasonDetails = await $fetch<TVSeasonDetailsResponse>(`/api/tv/${showId}/season/${seasonNumber}`);
+        },
+        async fetchSimilarTvShows(showId: number) {
+            this.similarTvShows = await $fetch<TMDBSimilarTvShowsResponse>(`/api/tv/${showId}/similar`);
         },
     },
 });
---
+++ types/season.ts
@@ -0,0 +1,53 @@
+export interface TVSeasonDetailsResponse extends TVSeasonDetails {}
+
+interface TVSeasonDetails {
+    _id: string;
+    air_date: string;
+    episodes: Episode[];
+    name: string;
+    overview: string;
+    id: number;
+    poster_path: string;
+    season_number: number;
+    vote_average: number;
+}
+
+interface Episode {
+    air_date: string;
+    episode_number: number;
+    id: number;
+    name: string;
+    overview: string;
+    production_code: string;
+    runtime: number;
+    season_number: number;
+    show_id: number;
+    still_path: string;
+    vote_average: number;
+    vote_count: number;
+    crew: CrewMember[];
+    guest_stars: GuestStar[];
+}
+
+interface CrewMember extends Person {
+    department: string;
+    job: string;
+    credit_id: string;
+}
+
+interface GuestStar extends Person {
+    character: string;
+    credit_id: string;
+    order: number;
+}
+
+interface Person {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+}
