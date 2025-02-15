4d14c2a120dab7757984a5bcf8cda4301bbad721
Michael Eder <eder.michi98@gmx.at>
2025-02-15 15:31:29 +0100

    fix: delete AccountButton component

--- components/AccountButton.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <NuxtLink to="/account" class="pointer-events-auto button">
-        <span class="i-ph-user-bold size-6" />
-    </NuxtLink>
-</template>
-
-<script lang="ts" setup></script>
-
-<style></style>
