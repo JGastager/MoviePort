f48ea6d832569e523dca86b5773a31092de9d401
Michael Eder <eder.michi98@gmx.at>
2025-05-25 18:23:51 +0200

    fix: i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["nuxt-electron", "@unocss/nuxt", "@nuxt/eslint", "@nuxt/fonts", "@morev/vue-transitions/nuxt", "@pinia/nuxt", "@nuxtjs/i18n", "nuxt-pages-plus"],
+    modules: ["nuxt-pages-plus", "nuxt-electron", "@unocss/nuxt", "@nuxt/eslint", "@nuxt/fonts", "@morev/vue-transitions/nuxt", "@pinia/nuxt", "@nuxtjs/i18n"],
     ssr: false,
     devtools: { enabled: true },
     app: {
@@ -53,17 +53,12 @@ export default defineNuxtConfig({
         },
     },
     i18n: {
-        // vueI18n: "./i18n.config.ts",
+        defaultLocale: "en",
+        lazy: true,
         strategy: "no_prefix",
         locales: [
-            {
-                code: "en",
-                name: "English",
-            },
-            {
-                code: "de",
-                name: "Deutsch",
-            },
+            { code: "en", name: "English", file: "en.json" },
+            { code: "de", name: "Deutsch", file: "de.json" },
         ],
         skipSettingLocaleOnNavigate: true,
         detectBrowserLanguage: {
@@ -72,8 +67,5 @@ export default defineNuxtConfig({
             alwaysRedirect: false,
             fallbackLocale: "en",
         },
-    },
-    pagesPlus: {
-        // Nuxt Pages Plus configuration
     },
 });
